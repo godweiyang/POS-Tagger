df436f3df92660af3ce81596d7beb9de78ba12cf
godweiyang <792321264@qq.com>
2018-08-13 18:19:40 +0800

    init files

--- src/POStag.py
+++ src/POStag.py
@@ -6,6 +6,39 @@ import numpy as np
 UNK = "<UNK>"
 
 
+class Feedforward(object):
+    def __init__(self, model, input_dim, hidden_dims, output_dim):
+        self.spec = locals()
+        self.spec.pop("self")
+        self.spec.pop("model")
+
+        self.model = model.add_subcollection("Feedforward")
+
+        self.weights = []
+        self.biases = []
+        dims = [input_dim] + hidden_dims + [output_dim]
+        for prev_dim, next_dim in zip(dims, dims[1:]):
+            self.weights.append(
+                self.model.add_parameters((next_dim, prev_dim)))
+            self.biases.append(self.model.add_parameters(next_dim))
+
+    def param_collection(self):
+        return self.model
+
+    @classmethod
+    def from_spec(cls, spec, model):
+        return cls(model, **spec)
+
+    def __call__(self, x):
+        for i, (weight, bias) in enumerate(zip(self.weights, self.biases)):
+            weight = dy.parameter(weight)
+            bias = dy.parameter(bias)
+            x = dy.affine_transform([bias, weight, x])
+            if i < len(self.weights) - 1:
+                x = dy.rectify(x)
+        return x
+
+
 class OutputLayer(object):
     def __init__(self, model, input_dim, hidden_dim, output_dim):
         self.spec = locals()
@@ -86,7 +119,10 @@ class MultiHeadSelfAttentive(object):
         self.spec.pop("model")
 
         self.model = model.add_subcollection("MultiHeadSelfAttentive")
+        self.d_model = d_model
 
+        self.feedforward = Feedforward(
+            self.model, self.d_model, [self.d_model], self.d_model)
         self.attention = []
         for i in range(8):
             self.attention.append(SingleHeadSelfAttentive(
@@ -100,10 +136,50 @@ class MultiHeadSelfAttentive(object):
         return cls(model, **spec)
 
     def __call__(self, X):
+        d_x = X.dim()[0][0]
+        d_y = X.dim()[0][1]
+        g = dy.ones((d_x, d_y))
+        b = dy.zeros((d_x, d_y))
         Y = []
         for attention in self.attention:
             Y.append(attention(X))
-        return dy.esum(Y)
+        Y = dy.esum(Y)
+        Y = dy.layer_norm(X + Y, g, b)
+        Y = dy.layer_norm(
+            Y + dy.transpose(self.feedforward(dy.transpose(Y))), g, b)
+        return Y
+
+
+class AttentionEncoder(object):
+    def __init__(
+        self,
+        model,
+        d_model,
+        d_k,
+        d_v,
+    ):
+        self.spec = locals()
+        self.spec.pop("self")
+        self.spec.pop("model")
+
+        self.model = model.add_subcollection("AttentionEncoder")
+
+        self.attention = []
+        for i in range(8):
+            self.attention.append(MultiHeadSelfAttentive(
+                self.model, d_model, d_k, d_v))
+
+    def param_collection(self):
+        return self.model
+
+    @classmethod
+    def from_spec(cls, spec, model):
+        return cls(model, **spec)
+
+    def __call__(self, X):
+        for attention in self.attention:
+            X = attention(X)
+        return X
 
 
 class BiLSTMTagger(object):
@@ -273,7 +349,7 @@ class AttentionTagger(object):
             self.model,
             dy.VanillaLSTMBuilder)
 
-        self.attention = MultiHeadSelfAttentive(
+        self.attention = AttentionEncoder(
             self.model,
             2 * char_lstm_dim + word_embedding_dim + pos_embedding_dim,
             2 * char_lstm_dim + word_embedding_dim + pos_embedding_dim,
